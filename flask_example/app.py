2362514d806aabcc225e6b40806b85ddc6095d6c
Javier Gonzalez <javierggt@yahoo.com>
2022-01-31 19:54:35 -0500

    step 2: getting the input parameters

--- flask_example/app.py
+++ flask_example/app.py
@@ -3,10 +3,10 @@
 """
 """
 
-from flask import Flask
+from flask import Flask, request
+from flask_restful import reqparse
 
 from mica.archive import obspar
-
 
 app = Flask('flask_tutorial')
 
@@ -17,7 +17,14 @@ def get_obspar():
     """
     Service to return observation parameters.
     """
-    pars = obspar.get_obspar(8008)
+    # unfortunately deprecated (https://flask-restful.readthedocs.io/en/latest/reqparse.html)
+    # but I use it.
+    # parser = reqparse.RequestParser()
+    # parser.add_argument('obsid', help='ID of the observation', type=int, default=0)
+    # args = parser.parse_args()
+    # obsid = args.obsid
+    obsid = int(request.form.get('obsid', request.values.get('obsid', 0)))
+    pars = obspar.get_obspar(obsid)
 
     return {'obspar': pars}, 200
 
